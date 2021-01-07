6103c74ef94db6fa800e29c3a6b62c1bb98e666d
zeliha konak <zelihaozsiray@gmail.com>
2021-01-07 10:35:27 +0100

    exercise4

--- exercise4.js
+++ exercise4.js
@@ -1,11 +1,17 @@
 function showArrayElements(param){
+   
+if(param!=undefined){
     for(let index=0;index<param.length;index++){
         console.log(param[index]);
     }
     return param.length;
+} else{
+    return 0 ;
+}
 }
 
+var count=showArrayElements([5,7,8,9]);
+console.log("count:"+count);
 
-
-var count=showArrayElements([2,5,6,8]);
+count=showArrayElements();
 console.log("count:"+count);
